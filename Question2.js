fe6205239c3dc2028073dfa78967676070948d6f
Allyrion <kishan.solomon@gmail.com>
2021-11-21 18:27:05 -0400

    Finished Question 2

--- Question2.js
+++ Question2.js
@@ -1,4 +1,13 @@
 /*Question 2*/
 
-console.log("We are going to calculate the area of a circle given the radius!")
-const area = parseInt(prompt("Please give us the radius"))
+const prompt = require("prompt-sync")();
+
+console.log("We are going to calculate both the area and the circumference of a circle given the radius!");
+const radius = parseInt(prompt("Please give us the radius: "));
+const pi = Math.PI;
+
+const area = pi * (radius**2);
+
+const circumference = 2 * pi * radius;
+
+console.log(`The area of the circle is ${area} and the circumference is ${circumference}`);
