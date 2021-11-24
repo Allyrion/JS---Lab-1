5d0470ff7feb43c46640164070b7712581f5e19f
Allyrion <kishan.solomon@gmail.com>
2021-11-24 00:08:25 -0400

    Completed Question 8

---
+++ Question8.js
@@ -0,0 +1,29 @@
+/*Question 6*/
+
+const prompt = require("prompt-sync")();
+
+const userName = prompt("Hi, what's your name? ");
+console.log(`Welcome to our show, ${userName}!`);
+let age = prompt("How old are you? ");
+
+if (isNaN(age) === true) {
+    console.log("Sorry, you did not enter a valid number.");
+}
+else {
+    let userAge = parseInt(age);
+    const mamaguyAgeDown = userAge - 5;
+    const mamaguyAgeUp = userAge + 5;
+
+    if (userAge < 1) {
+    console.log("Sorry, your age cannot be zero or a negative number!");
+    }
+    else if (userAge < 10) {
+        console.log("Sorry, you must be older to proceed");
+    }
+    else if (userAge >= 10 && userAge < 23) {
+        console.log(`Wow, you seem quite mature! I thought you were ${mamaguyAgeUp}!`);
+    }
+    else {
+        console.log(`Wow, you look quite young for your age! I thought you were ${mamaguyAgeDown}!`);
+    }
+}
